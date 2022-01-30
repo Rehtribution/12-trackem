79cbb8f27c2fd5a1101559d493ca08ce46d6f15e
Reht Cavazos <reht.dev@gmail.com>
2022-01-29 23:34:28 -0600

    removed routes as they were not needed and were taking too long to debug. added starting prompt.

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ const consTable = require("console.table");
 
 
 const express = require("express");
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 9001;
 const app = express();
-
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: "localhost",
-    port: 3306,
-    
+    port: 9001,
+
     // placeholder username and password
     user: 'root',
     password: '',
@@ -24,19 +26,77 @@ db.connect((err) => {
     if (err) {
         throw err;
     }
-    console.log("Connected!");
+    console.log("Connected to the Employee Database!");
+    // begin questions
+    startPrompt();
 });
 
-// create new db
-app.get('/newdb', (req, res) => {
-    let sql = `CREATE DATABASE emp`;
-        res.send('database created');
-    });
 
-app.get('/employeetable', (req, res) => {
-    let sql = `CREATE TABLE employee(id int AUTO_INCREMENT PRIMARY KEY, name VARCHAR(40), position VARCHAR(40),`;
-    res.send('employee table created');
-})
+function startPrompt() {
+    inquirer.prompt({
+        type: "list",
+        name: "task",
+        message: "What can I help you with?",
+        choices: [
+            "View Departments",
+            "View Roles",
+            "View Employees",
+
+            "Add New Department",
+            "Add New Role",
+            "Add New Employee",
+
+            "Update An Employee's Role",
+
+            "Delete A Department",
+            "Delete A Role",
+            "Delete An Employee",
+
+            "End"
+        ],
+    })
+        .then((selection) => {
+            switch (selection) {
+                case "View Departments":
+                    viewDep();
+                case "View Roles":
+                    viewRol();
+                case "View Employees":
+                    viewEmp();
+                break;
+
+                case "Add New Department":
+                    addDep();
+                case "Add New Role":
+                    addRol()
+                case "Add New Employee":
+                    addEmp()
+                break;
+
+                case "Update An Employee's Role":
+                    updateEmp();
+                    break;
+
+                case "Delete A Department":
+                    delDep();
+                case "Delete A Role":
+                    delRol();
+                case "Delete An Employee":
+                    delEmp();
+                    break;
+
+                case "End":
+                    fin();
+            }
+        });
+};
+
+// FUNCTIONS
+
+function viewDep() {
+    console.log("Departments");
+    
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
